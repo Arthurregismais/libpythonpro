43f9b51f9ee869056dd5b595d892462c27351471
Arthur <arthur.regismais@hotmail.com>
2021-12-15 18:31:50 -0300

    Estudado o conceito do pytest parametrize. Ainda não implementada a cobertura de testes com o Codecov

---
+++ MeuPrimeiroPacoteLibpythonpro/tests/tests_spam/test_enviador_de_email.py
@@ -0,0 +1,22 @@
+import pytest
+
+from MeuPrimeiroPacoteLibpythonpro.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    enviador is not None
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['arthur.regismais@hotmail.com', 'andressamdm.@hotmail.com'] )
+def test_remetente(destinatario):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+            destinatario,
+            'adevar.mais@hotmail.com',
+            'Fotos das obras da firma',
+            'Ta faltando fotos mais recentes.'
+        )
+    assert destinatario in resultado
